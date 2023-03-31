b1d127f2a69dc1cf7fde8d27a25768fea9ceb892
Manal-ser <manal_ser@hotmail.com>
2023-03-30 19:37:32 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,13 +10,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i, j;
-
-	i = 0;
+	int i = 0;
+	int j = 0;
 
 	while (dest[i] != '\0')
 		i++;	
-	j = 0;
+	
 	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
