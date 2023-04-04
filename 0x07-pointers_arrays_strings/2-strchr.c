be14725b6eae796dd554ef64d5797139be672be2
Manal-ser <manal_ser@hotmail.com>
2023-04-03 18:45:26 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to the first occurence of the char c
+ * @c: char to look for
+ * Return: pointer s
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] = c)
+			return (s[i]);
+	}
+	return (0);
+}	
