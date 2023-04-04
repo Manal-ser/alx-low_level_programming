b97288de2ffd4aeb5fdab78ceb828cc43d364117
Manal-ser <manal_ser@hotmail.com>
2023-04-03 18:29:39 -0700

    memset

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,13 @@
  * @s: pointer
  * @b: constant
  * @n: memory area
- *
  * Return: a pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++; n--)
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	return (s)
+	return (s);
 }
