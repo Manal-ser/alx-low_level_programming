07370fd24375ae94ea83ae6c0678dfa3fe0546f1
Manal-ser <manal_ser@hotmail.com>
2023-04-05 19:10:19 -0700

    Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,12 +6,8 @@
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
-
 	if (*s != '\0')
-	{
-		i++;
-		_strlen_recursion(s + 1);
-	}
-	return (i);
+		return (_strlen_recursion(s + 1) + 1);
+	else
+		return (0);
 }
