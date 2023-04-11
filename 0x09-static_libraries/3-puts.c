5f1e5061f8769e4d928e3dd8d3fb265801fc7455
Manal-ser <manal_ser@hotmail.com>
2023-04-10 17:33:17 -0700

    object files and library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,16 +1,10 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - prints a string, followed by a new line
- * to stdout
  * @str: string input
- *
- * Return: Nothing
  */
 void _puts(char *str)
 {
-	int i;
-
-	for (i = 0; str[i] != '\0'; i++)
-		_putchar(str[i]);
-	_putchar('\n');
+	puts(str);
 }
