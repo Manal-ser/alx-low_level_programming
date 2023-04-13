2c796dd63933cc8ba2aaf1bcf7ee8b086f5600c7
Manal-ser <manal_ser@hotmail.com>
2023-04-13 09:50:35 -0700

    string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,7 +33,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
 
-	for (j = 0; j < n && s2[j] != '\0')
+	for (j = 0; j < n && s2[j] != '\0', j++)
 	{
 		ptr[i] = s2[j];
 		i++;
