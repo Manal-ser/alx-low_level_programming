27a57fd6f40bad08c85f48b3a607b82507819150
Manal-ser <manal_ser@hotmail.com>
2023-04-13 19:11:45 -0700

    _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,17 +9,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *ptr;
+	char *s;
 	unsigned int i;
 
-	if (nmemb == 0 || size ==0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
-
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	s = malloc(nmemb * size);
+	if (s == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
-	return (ptr);
+	for (i = 0; i < nmemb * size; i++)
+	{
+		s[i] = 0;
+	}
+	return (s);
 }
