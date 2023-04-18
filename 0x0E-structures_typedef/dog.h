2ecb91486ee3ecd233dcc98c12ce2de96c6ce027
Manal-ser <manal_ser@hotmail.com>
2023-04-17 18:08:21 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,7 +3,6 @@
 
 /**
  * struct dog - structure of dog
- *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
@@ -18,7 +17,6 @@ struct dog
 /**
  * dog_t - typedef of struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
