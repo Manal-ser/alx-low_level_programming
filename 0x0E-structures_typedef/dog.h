4466798a7711fd52d132bcd75bd4bacbb26d3a6f
Manal-ser <manal_ser@hotmail.com>
2023-04-17 18:05:12 -0700

    dog_h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,14 +1,13 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef dog_h
+#define dog_h
 
 /**
- * struct dog - struct of a dog
+ * struct dog - structure of dog
  *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
-
 struct dog
 {
 	char *name;
