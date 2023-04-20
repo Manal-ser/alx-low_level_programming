b17a978ee5638ed194e97ab6d217ff2a43c79c51
Manal-ser <manal_ser@hotmail.com>
2023-04-19 19:29:13 -0700

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,9 +2,8 @@
 
 /**
  * get_op_func - selects the correct function to perform the operation
- *  asked by the user
- * @s: the operator passed as argument to the program
- * 
+ * asked by the user
+ * @s: the operator passed as argument to the program 
  * Return: pointer to the right function
  */
 int (*get_op_func(char *s))(int, int)
@@ -17,9 +16,7 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (i < 5)
 	{
