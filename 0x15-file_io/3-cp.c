ededbea81c7d8f8abb2ca33d8ddebf7ddc83ca1b
Manal-ser <manal_ser@hotmail.com>
2023-05-08 18:22:02 -0700

    ya rbi ykhdem

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+/**
+ * error_file - check if file is accessible
+ * @filename: filename
+ * @file: file
+ * @argv: arguments
+ * Return:Nothing
+ */
+void error_file(int filename, int file, char *argv[])
+{
+	if (filename == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (file == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+
+/**
+ * main - checks the code
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int source, dest, on_close, r, w;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest == -1)
+		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+
+	source = open(argv[1], O_RDONLY);
+	if (source == -1)
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+
+	while (1)
+	{
+		r = read(source, buffer 1024);
+		if (r == -1)
+			dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
+		if (r > 0)
+		{
+			w = write(dest, buffer, r);
+			if (w == -1)
+				dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+		}
+		else
+				break;
+	}
+		on_close = close(source);
+		if (on_close == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source), exit(100);
+		on_close = close(dest);
+		if (on_close == -1)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
+		return (0);
+}
